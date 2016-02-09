c58074bd523c2f9c0c99f449db0ccf730d21221c
Zabi <mzabiulla@broadviewnet.com>
2016-02-09 17:28:52 +0530

    Basic exercise 1

---
+++ cpp/basics/exercise_1/main.cpp
@@ -0,0 +1,36 @@
+
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+/**
+ * @brief  
+ * 	program that asks the user to type an integer and writes "YOU WIN"
+ *	if the value is between 56 and 78 (both included). 
+ *	In the other case it writes "YOU LOSE".
+ * 
+ * @return 
+ * 	int
+ */
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) 
+    {
+        cout << "Usage ./exercise1 number" << endl;
+	return 0;
+    }
+    
+    int number = atoi(argv[1]);
+    if ( number >= 56 && number <= 78) 
+    {
+	cout << "YOU WIN" << endl;
+    } else 
+    {
+	cout << "YOU LOSE" << endl;
+    }
+      
+    return 0;
+}
